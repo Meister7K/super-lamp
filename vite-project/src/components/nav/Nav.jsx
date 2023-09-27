6978ccdbadaaa2174abf6c5dab666c5c3a1ca5c6
Karl Finkel <kmanfinkel17@gmail.com>
2023-09-27 16:15:01 -0600

    fixed nav icon placement and ham

--- vite-project/src/components/nav/Nav.jsx
+++ vite-project/src/components/nav/Nav.jsx
@@ -4,22 +4,40 @@ import { useState } from 'react'
 
 export const Nav = () => {
 
-    const [active, setActive]= useState(null)
+    const [active, setActive] = useState(null)
+    const [toggle, setToggle] = useState(false)
 
     const location = useLocation();
 
+    const toggleMenu = () => {
+        if (toggle === false) {
+            setToggle(true)
+        } else {
+            setToggle(false)
+        }
+
+    }
+
     return (
         <nav>
-            <ul className='nav-ul'>
+            <button className='hamburger' onClick={() => toggleMenu()}>
+                {toggle === false ? (<svg xmlns="http://www.w3.org/2000/svg" className='ham-icon' width='100%' height='100%' fill="currentColor" viewBox='0 0 16 16' >
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>) : (<svg xmlns="http://www.w3.org/2000/svg" className='ham-icon' width='100%' height='100%' fill="currentColor" viewBox='0 0 16 16'>
+                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>)}
+            </button>
+            <ul className={`nav-ul ${toggle === true ? 'visible' : 'hidden'}`}>
                 <li>
-                    <NavLink to='/' className={`nav-link ${active === '/'?'acive':''}`}>
+                    <NavLink to='/' className={`nav-link ${active === '/' ? 'active' : ''}`}>
                         <span className='nav-name'>Home</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='nav-icon' viewBox="0 0 16 16">
                             <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z" />
                         </svg></NavLink>
                 </li>
                 <li>
-                    <NavLink to='/projects' className={`nav-link ${active === '/projects'?'acive':''}`}>
+                    <NavLink to='/projects' className={`nav-link ${active === '/projects' ? 'active' : ''}`}>
                         <span className='nav-name'>Projects</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='nav-icon' viewBox="0 0 16 16">
                             <path d="M2 4a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v11.5a.5.5 0 0 1-.777.416L7 13.101l-4.223 2.815A.5.5 0 0 1 2 15.5V4zm2-1a1 1 0 0 0-1 1v10.566l3.723-2.482a.5.5 0 0 1 .554 0L11 14.566V4a1 1 0 0 0-1-1H4z" />
@@ -29,7 +47,7 @@ export const Nav = () => {
                 </li>
                 <li>
 
-                    <NavLink to='/about' className={`nav-link ${active === '/about'?'acive':''}`}>
+                    <NavLink to='/about' className={`nav-link ${active === '/about' ? 'active' : ''}`}>
                         <span className='nav-name'>About</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='nav-icon' viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
@@ -40,7 +58,7 @@ export const Nav = () => {
                 </li>
                 <li>
 
-                    <NavLink to='/blog' className={`nav-link ${active === '/blog'?'acive':''}`}>
+                    <NavLink to='/blog' className={`nav-link ${active === '/blog' ? 'active' : ''}`}>
                         <span className='nav-name'>Blog</span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='nav-icon' viewBox="0 0 16 16">
                             <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z" />
