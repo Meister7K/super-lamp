ba52d7e338778175b03e2894b3bbbc5adea8fc12
Karl Finkel <kmanfinkel17@gmail.com>
2023-10-03 11:56:22 -0600

    Updates

---
+++ assets/About-1c2d64bf.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-ecf07f9f.js";function e(){return t.jsxs(t.Fragment,{children:[t.jsx("h1",{children:"About"}),t.jsx("p",{children:"This is the about me"})]})}export{e as default};
---
+++ assets/Projects-a0d4d665.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-ecf07f9f.js";function e(){return r.jsx(r.Fragment,{children:r.jsx("h1",{children:"Projects"})})}export{e as default};
