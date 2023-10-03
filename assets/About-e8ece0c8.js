6ea8912d92769b700607fd3af9970e1c1da50993
Karl Finkel <kmanfinkel17@gmail.com>
2023-10-02 20:02:49 -0600

    Updates

---
+++ assets/About-e8ece0c8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-43b3bf3b.js";function e(){return t.jsxs(t.Fragment,{children:[t.jsx("h1",{children:"About"}),t.jsx("p",{children:"This is the about me"})]})}export{e as default};
